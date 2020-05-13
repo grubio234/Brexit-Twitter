9602a4c7c2cbe37d5ee58077cdd443503179eea5
Jonas Kunath <jonas.kunath@gmx.de>
2020-05-13 19:37:26 +0200

    Extend  configuration mechanism to default/custom.

--- config/config.py
+++ config/config.py
@@ -1,9 +1,27 @@
-# platform
-import platform
+"""
+Import some configuration options from a custom configuration file.
+Compare them, or fall back to a default config file.
+The importing mechanism employed here is rather hacky and involves  importing
+the same module twice.
+"""
 
-if platform.system() == "Windows":
-    import sys
-    sys.path.insert(1, r'C:\Users\jonas\OneDrive\ETH\DataAna\vaderFolder')
-    data_dir = r'C:\Users\jonas\OneDrive\ETH\DataAna'+'\\'
+import os.path
+
+import config.default_config
+
+def checkIsValidCustomConfigModule(config_module):
+    for module_object_name in dir(config.default_config):
+        if module_object_name not in dir(config_module):
+            raise Exception("The custom configuration file {} does not contain "
+                "all necessary objects. The object '{}' is missing."
+                "".format(config_module, module_object_name))
+    print("A custom configuration module is imported.")
+
+
+custom_config_file = "config/custom_config.py"
+if os.path.isfile(custom_config_file):
+    import config.custom_config as cc
+    checkIsValidCustomConfigModule(cc)
+    from config.custom_config import *
 else:
-    data_dir = "data_git/"
+    from config.default_config import *
